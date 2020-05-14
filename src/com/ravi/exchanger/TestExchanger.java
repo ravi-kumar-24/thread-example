37d3b2d6af612edb2b6d20bc963526bc07949255
ravi-kumar-24 <hiravi2410@gmail.com>
2020-05-14 20:03:15 +0530

    Added code for exchanger

---
+++ src/com/ravi/exchanger/TestExchanger.java
@@ -0,0 +1,20 @@
+package com.ravi.exchanger;
+
+import java.util.concurrent.Exchanger;
+
+public class TestExchanger {
+
+    public static void main(String[] args) {
+
+        Exchanger exchanger = new Exchanger();
+
+        ExchangerRunnable exchangerRunnable1 =
+                new ExchangerRunnable(exchanger, "A");
+
+        ExchangerRunnable exchangerRunnable2 =
+                new ExchangerRunnable(exchanger, "B");
+
+        new Thread(exchangerRunnable1).start();
+        new Thread(exchangerRunnable2).start();
+    }
+}
